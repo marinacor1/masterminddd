a4f63cea604b25188ac254b969ddb4bde7938f59
marinacor1 <marinaspam1234@gmail.com>
2016-02-04 21:25:34 -0700

    deleted extra file

--- prompts.rb
+++
@@ -1,9 +0,0 @@
-module Prompts
-
-
-  def self.player_guess
-    puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?"
-  end
-
-  # def
-end
